11ff9b9b7cc6b3454e266634ba094d0c918b5aba
maaaru09 <h.akimaru08@gmail.com>
2024-03-04 19:46:12 +0900

    SinatraメモアプリのDB版です

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem "sinatra-contrib", "~> 4.0"
 gem "rackup", "~> 2.1"
 gem "erb_lint", require: false
 gem "parser", "3.2.2"
+gem "pg"
--- memo.rb
+++ memo.rb
@@ -2,7 +2,7 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
+require 'pg'
 
 helpers do
   def h(text)
@@ -10,19 +10,17 @@ helpers do
   end
 end
 
-def memos
-  File.open('public/memos.json', 'r') { |file| JSON.parse(file.read) }
+def connect_db
+  PG.connect(dbname: 'my_memo')
 end
 
-def write_memos(memos)
-  File.open('public/memos.json', 'w') do |file|
-    file.write(JSON.pretty_generate(memos))
-  end
+def memo_id
+  params[:memo_id].to_i
 end
 
 def show_memos
-  @memo_id = params[:memo_id]
-  @memo = memos[@memo_id]
+  result = connect_db.exec_params('SELECT * FROM memos WHERE id = $1', [memo_id])
+  @memo = result.first
 end
 
 get '/' do
@@ -30,7 +28,7 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = memos
+  @memos = connect_db.exec('SELECT * FROM memos ORDER BY id')
   erb :top
 end
 
@@ -49,34 +47,24 @@ get '/memos/:memo_id/edit' do
 end
 
 post '/memos/new' do
-  memo = memos
+  title = params['title']
+  content = params['content']
 
-  latest_key = memo.keys.map(&:to_i).max + 1
-  memo[latest_key] = {
-    'title' => params[:title],
-    'content' => params[:content]
-  }
-
-  write_memos(memo)
+  max_id = connect_db.exec('SELECT MAX(id) FROM memos').getvalue(0, 0).to_i
+  new_id = max_id + 1
+  connect_db.exec_params('INSERT INTO memos VALUES ($1, $2, $3)', [new_id, title, content])
   redirect '/'
 end
 
 patch '/memos/:memo_id' do
-  memo = memos
+  title = params['title']
+  content = params['content']
 
-  memo[params[:memo_id]] = {
-    'title' => params[:title],
-    'content' => params[:content]
-  }
-
-  write_memos(memo)
+  @memo = connect_db.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [title, content, memo_id])
   redirect '/'
 end
 
 delete '/memos/:memo_id' do
-  memo = memos
-
-  memo.delete(params[:memo_id])
-  write_memos(memo)
+  @memo = connect_db.exec_params('DELETE FROM memos WHERE id = $1', [memo_id])
   redirect '/'
 end
