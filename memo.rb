73ff31a443d5d22b08320916696bc5743bd8ec13
maaaru09 <h.akimaru08@gmail.com>
2024-03-05 18:03:27 +0900

    connect_dbメソッドを定数に変更しました

--- memo.rb
+++ memo.rb
@@ -4,23 +4,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
 
+CONNECT_DB = PG.connect(dbname: 'my_memo')
+
 helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
 end
 
-def connect_db
-  PG.connect(dbname: 'my_memo')
-end
-
-def memo_id
-  params[:memo_id].to_i
-end
-
-def show_memos
-  result = connect_db.exec_params('SELECT * FROM memos WHERE id = $1', [memo_id])
-  @memo = result.first
+def show_memo
+  memo_id = params[:memo_id].to_i
+  CONNECT_DB.exec_params('SELECT * FROM memos WHERE id = $1', [memo_id])
 end
 
 get '/' do
@@ -28,7 +22,7 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = connect_db.exec('SELECT * FROM memos ORDER BY id')
+  @memos = CONNECT_DB.exec('SELECT * FROM memos ORDER BY id')
   erb :top
 end
 
@@ -37,12 +31,12 @@ get '/memos/new' do
 end
 
 get '/memos/:memo_id' do
-  show_memos
+  @memo = show_memo.first
   erb :show
 end
 
 get '/memos/:memo_id/edit' do
-  show_memos
+  @memo = show_memo.first
   erb :edit
 end
 
@@ -50,21 +44,22 @@ post '/memos/new' do
   title = params['title']
   content = params['content']
 
-  max_id = connect_db.exec('SELECT MAX(id) FROM memos').getvalue(0, 0).to_i
-  new_id = max_id + 1
-  connect_db.exec_params('INSERT INTO memos VALUES ($1, $2, $3)', [new_id, title, content])
+  CONNECT_DB.exec_params('INSERT INTO memos (title, content) VALUES ($1, $2)', [title, content])
   redirect '/'
 end
 
 patch '/memos/:memo_id' do
   title = params['title']
   content = params['content']
+  memo_id = params[:memo_id].to_i
 
-  @memo = connect_db.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [title, content, memo_id])
+  @memo = CONNECT_DB.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [title, content, memo_id])
   redirect '/'
 end
 
 delete '/memos/:memo_id' do
-  @memo = connect_db.exec_params('DELETE FROM memos WHERE id = $1', [memo_id])
+  memo_id = params[:memo_id].to_i
+
+  @memo = CONNECT_DB.exec_params('DELETE FROM memos WHERE id = $1', [memo_id])
   redirect '/'
 end
